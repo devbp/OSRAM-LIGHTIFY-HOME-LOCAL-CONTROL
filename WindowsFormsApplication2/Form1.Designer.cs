7534152f50c2150355ba9005841be9b3ae1a04c1
Dev Poudel <d.poudel@osram.com>
2017-04-25 15:04:40 +0200

    LIGHTIFY home GW local control

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "192.168.2.106";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "ON ALL LAMPS";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(24, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "OFF ALL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 368);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "LIGHTIFY HOME";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private static TcpClient client;
+        private NetworkStream nwStream;
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            ConnectClient(textBox1.Text);
+          
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ConnectClient(string machineName)
+        {
+           
+            client = new TcpClient();
+            try
+            {
+                client.Connect(machineName, 4000);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Error in connection, verify your ip of GW",e.ToString());
+
+            }
+           
+            if (client.Connected)
+            {
+                textBox1.Visible = false;
+                button1.Visible = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+
+                nwStream = client.GetStream();
+             
+               
+
+            }
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] msg = new byte[] { 0x0F, 0x00, 0x00, 0x32, 0x03, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01 };
+           
+            nwStream.Write(msg, 0, msg.Length);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] msg = new byte[] { 0x0F, 0x00, 0x00, 0x32, 0x03, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00 };
+
+            nwStream.Write(msg, 0, msg.Length);
+
+        }
+    }
+}
